c9bd486804c75fc112383b4dbac411273fe5fddb
David Pietrocola <pietrocoladavid@gmail.com>
2024-10-10 23:17:45 -0400

    style: decouple top navigator

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
-import DeployButton from "@/components/deploy-button";
-import { EnvVarWarning } from "@/components/env-var-warning";
 import { Footer } from "@/components/footer";
-import HeaderAuth from "@/components/header-auth";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
-import Link from "next/link";
 import "./globals.css";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { TopNavigator } from "@/components/top-nav";
 
 const defaultUrl = process.env.VERCEL_URL
 	? `https://${process.env.VERCEL_URL}`
@@ -35,17 +31,7 @@ export default function RootLayout({
 				>
 					<main className="min-h-screen flex flex-col items-center">
 						<div className="flex-1 w-full flex flex-col items-center">
-							<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-								<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-									<div className="flex gap-5 items-center font-semibold">
-										<Link href={"/"}>Next.js Supabase Starter</Link>
-										<div className="flex items-center gap-2">
-											<DeployButton />
-										</div>
-									</div>
-									{!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-								</div>
-							</nav>
+							<TopNavigator />
 							<div className="flex flex-col">{children}</div>
 
 							<Footer />
