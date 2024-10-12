ba101b4a79d5e466ea3166017cf342b08d74b6bd
David Pietrocola <pietrocoladavid@gmail.com>
2024-10-11 23:06:33 -0400

    feat: improves submit button

--- components/submit-button.tsx
+++ components/submit-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
 import type { ComponentProps } from "react";
 import { useFormStatus } from "react-dom";
 
@@ -13,11 +14,22 @@ export function SubmitButton({
 	pendingText = "Submitting...",
 	...props
 }: Props) {
+	// https://react.dev/reference/react-dom/hooks/useFormStatus
 	const { pending } = useFormStatus();
 
 	return (
 		<Button type="submit" aria-disabled={pending} {...props}>
-			{pending ? pendingText : children}
+			{pending ? (
+				<div className="flex gap-1">
+					<Loader2
+						className="animate-spin text-foreground-muted mt-0.5"
+						size={16}
+					/>
+					<span>{pendingText}</span>
+				</div>
+			) : (
+				children
+			)}
 		</Button>
 	);
 }
