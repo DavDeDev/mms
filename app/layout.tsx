46a9b071a1d6f6164340b1b5e40872de85d94aad
David Pietrocola <pietrocoladavid@gmail.com>
2024-10-12 03:38:15 -0400

    feat: change metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
 	metadataBase: new URL(defaultUrl),
-	title: "Next.js and Supabase Starter Kit",
-	description: "The fastest way to build apps with Next.js and Supabase",
+	title: "Mentoship Management System",
+	description: "Manage your Mentorhsip Program with ease.",
 };
 
 export default function RootLayout({
