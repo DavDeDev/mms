f9d8cff10b62b66dfbf6da792980002c2860784d
David Pietrocola <pietrocoladavid@gmail.com>
2024-10-12 03:30:08 -0400

    feat: improves sign-in and sign-up forms

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,23 +1,35 @@
-import { signInAction } from "@/actions/auth-actions";
+import { signInAction } from "@/actions/auth-actions"; // Adjust the import based on your file structure
 import { FormMessage, type Message } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-export default function Login({ searchParams }: { searchParams: Message }) {
+export default function SignIn({ searchParams }: { searchParams: Message }) {
+	if ("message" in searchParams) {
+		return (
+			<div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
+				<FormMessage message={searchParams} />
+			</div>
+		);
+	}
 	return (
-		<form className="flex-1 flex flex-col min-w-64">
+		<form className="flex flex-col w-64 mx-auto">
 			<h1 className="text-2xl font-medium">Sign in</h1>
-			<p className="text-sm text-foreground">
+			<p className="text-sm text text-foreground">
 				Don't have an account?{" "}
-				<Link className="text-foreground font-medium underline" href="/sign-up">
+				<Link className="text-primary font-medium underline" href="/sign-up">
 					Sign up
 				</Link>
 			</p>
 			<div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
 				<Label htmlFor="email">Email</Label>
-				<Input name="email" placeholder="you@example.com" required />
+				<Input
+					name="email"
+					placeholder="you@example.com"
+					type="email"
+					required
+				/>
 				<div className="flex justify-between items-center">
 					<Label htmlFor="password">Password</Label>
 					<Link
@@ -31,9 +43,10 @@ export default function Login({ searchParams }: { searchParams: Message }) {
 					type="password"
 					name="password"
 					placeholder="Your password"
+					minLength={6}
 					required
 				/>
-				<SubmitButton pendingText="Signing In..." formAction={signInAction}>
+				<SubmitButton formAction={signInAction} pendingText="Signing in...">
 					Sign in
 				</SubmitButton>
 				<FormMessage message={searchParams} />
