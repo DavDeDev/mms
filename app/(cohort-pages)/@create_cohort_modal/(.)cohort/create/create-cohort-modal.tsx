0d600ecaf2c997c7457d89bfea77eefa4494c055
David <pietrocoladavid@gmail.com>
2024-10-21 22:05:37 -0400

    Feature/create route interceptor (#6)
    * feat: adds route interceptor
    
    * feat: improves create cohort dialog
    
    * feat: modal dimension
    
    * feat: rename @cohort segment
    
    * feat: improves style a bit

---
+++ app/(cohort-pages)/@create_cohort_modal/(.)cohort/create/create-cohort-modal.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import CreateCohortForm from "@/app/(cohort-pages)/cohort/create/create-cohort-form";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { useRouter } from "next/navigation";
+
+export default function CreateCohortModal({
+	userEmail,
+}: { userEmail: string }) {
+	const router = useRouter();
+	return (
+		<Dialog open onOpenChange={() => router.back()}>
+			<DialogContent className="container">
+				<div className="container">
+				<CreateCohortForm userEmail={userEmail} />
+				</div>
+			</DialogContent>
+		</Dialog>
+	);
+}
---
+++ app/(cohort-pages)/layout.tsx
@@ -0,0 +1,16 @@
+const CohortsPageLayout = ({
+	children, 		
+	create_cohort_modal,
+}: {
+	children: React.ReactNode;
+	create_cohort_modal: React.ReactNode;
+}) => {
+	return (
+		<>
+			{children}
+			{create_cohort_modal}
+		</>
+	);
+};
+
+export default CohortsPageLayout;
