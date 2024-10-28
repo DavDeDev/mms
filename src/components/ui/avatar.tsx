47c040497356606af5de24f6976b6f9dbdac3422
David Pietrocola <pietrocoladavid@gmail.com>
2024-10-28 16:58:42 -0400

    feat: adds shadcn sidebar

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { cn } from "@/utils/cn";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 import * as React from "react";
+
+import { cn } from "@/utils/cn";
 
 const Avatar = React.forwardRef<
 	React.ElementRef<typeof AvatarPrimitive.Root>,
