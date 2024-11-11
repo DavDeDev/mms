fb5c2a2238d3ee9bbcd377a779ec7efe2f933ae6
David Pietrocola <pietrocoladavid@gmail.com>
2024-11-10 22:44:32 -0500

    fix: data table

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -3,7 +3,6 @@
 import {
 	type ColumnDef,
 	type ColumnFiltersState,
-	type FilterFn,
 	type SortingState,
 	type VisibilityState,
 	flexRender,
@@ -26,20 +25,8 @@ import {
 	TableRow,
 } from "./table";
 
-import { rankItem } from "@tanstack/match-sorter-utils";
 import { DataTablePagination } from "./data-table-pagination";
 import { DataTableToolbar } from "./data-table-toolbar";
-
-const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-	// Rank the item
-	const itemRank = rankItem(row.getValue(columnId), value);
-
-	// Store the itemRank info
-	addMeta({ itemRank });
-
-	// Return if the item should be filtered in/out
-	return itemRank.passed;
-};
 
 interface DataTableProps<TData, TValue> {
 	columns: ColumnDef<TData, TValue>[];
