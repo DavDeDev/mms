0760a1c7cdcadccb5b6342e42671b6819f3226c1
David Pietrocola <pietrocoladavid@gmail.com>
2024-11-10 23:25:07 -0500

    feat: new Copy Text component

--- src/components/tables/cohort-members-table/columns.tsx
+++ src/components/tables/cohort-members-table/columns.tsx
@@ -2,6 +2,7 @@
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
+import CopyTextComponent from "@/components/ui/copy-text";
 import { DataTableColumnHeader } from "@/components/ui/data-table-column-header";
 import type { getCohortMembers } from "@/queries/cached-queries";
 import { CohortRole } from "@/types/enums";
@@ -53,7 +54,7 @@ export const columns: ColumnDef<CohortMember>[] = [
 						<div className="font-medium">
 							{member.first_name} {member.last_name}
 						</div>
-						<div className="text-sm text-muted-foreground">{member.email}</div>
+						<CopyTextComponent>{member.email}</CopyTextComponent>
 					</div>
 				</div>
 			);
