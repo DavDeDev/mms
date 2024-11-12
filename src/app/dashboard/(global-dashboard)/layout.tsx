ed02eadcaacf0fd640ab5dd32063b25fd44014cf
David Pietrocola <pietrocoladavid@gmail.com>
2024-11-12 01:19:15 -0500

    fix: sidebar trigger not showing in mobile

--- src/app/dashboard/(global-dashboard)/layout.tsx
+++ src/app/dashboard/(global-dashboard)/layout.tsx
@@ -1,5 +1,9 @@
 import { GlobalDashboardSidebar } from "@/components/global-dashboard-sidebar";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import {
+	SidebarInset,
+	SidebarProvider,
+	SidebarTrigger,
+} from "@/components/ui/sidebar";
 
 export default function RootLayout({
 	children,
@@ -10,6 +14,9 @@ export default function RootLayout({
 		<SidebarProvider defaultOpen={true}>
 			<GlobalDashboardSidebar />
 			<SidebarInset>
+				<header className="md:hidden flex h-16 shrink-0 items-center gap-2 px-4">
+					<SidebarTrigger className="-ml-1" />
+				</header>
 				<div className="container my-4">{children}</div>
 			</SidebarInset>
 		</SidebarProvider>
